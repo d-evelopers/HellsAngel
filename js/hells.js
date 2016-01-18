0e81e6d6ca35f828f0c56744aba21204835ef3a6
thingywhat <thingywhat@live.com>
2016-01-17 21:42:25 -0700

    Move non-shared code to game.js

--- js/hells.js
+++ js/hells.js
@@ -1,14 +1,5 @@
 'use strict';
 
-// The modules
-const IPC = require('electron').ipcRenderer;
-const DevilGirl = require('./js/DevilGirl');
-
-// Populating game data from the server...eventually
-IPC.on('devil-girl-quotes', function(event, quotes){
-  const girl = new DevilGirl(quotes);
-});
-
 /**
  * Selects elements by selector and returns them in an Array.
  */
