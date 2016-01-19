d5e2b42ac3546152cc6d1e9453f18ea5be99dd84
thingywhat <thingywhat@live.com>
2016-01-18 21:58:11 -0700

    Allow saving JSON files from the reaction editor

--- js/reaction.js
+++ js/reaction.js
@@ -8,15 +8,32 @@ bind(window, function(){
       case (e.target.matches(".delete")):
         closest(e.target, "ul").removeChild(closest(e.target, "li"));
         break;
+
       case (e.target.matches(".addReaction")):
         let target = e.target.parentElement.querySelector(".reactions");
         target.appendChild(getSnippet('reaction'));
         target.querySelector("li:last-of-type > input").focus();
         break;
+
       case (e.target.matches(".addLevel")):
         e.target.parentElement.querySelector(".levels").appendChild(getSnippet('level'));
         document.querySelector(".level:last-of-type input").focus();
+        break;
+
       default:
     }
+  });
+
+  bind("#save", function(){
+    var json = JSON.stringify($$(".level").map(function(level){
+      return Array.prototype.map.call(level.querySelectorAll("input"), function(input){
+        return input.value;
+      });
+    }));
+
+    var link = document.createElement('a');
+    link.download = "reactions.json";
+    link.href = "data:text/json;charset=utf-8," + escape(json);
+    link.click();
   });
 }, 'onload');
