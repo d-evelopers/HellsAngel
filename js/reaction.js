a4bed6295bf76b4be02c47fcb6bde1e87f7752b8
thingywhat <thingywhat@live.com>
2016-01-18 21:12:19 -0700

    Made the reaction editor slightly nicer to use

--- js/reaction.js
+++ js/reaction.js
@@ -1,14 +1,21 @@
+'use strict';
+
 bind(window, function(){
+  document.querySelector("input").focus();
+
   bind("#editor", function(e){
     switch(true){
       case (e.target.matches(".delete")):
         closest(e.target, "ul").removeChild(closest(e.target, "li"));
         break;
       case (e.target.matches(".addReaction")):
-        e.target.parentElement.querySelector(".reactions").appendChild(getSnippet('reaction'));
+        let target = e.target.parentElement.querySelector(".reactions");
+        target.appendChild(getSnippet('reaction'));
+        target.querySelector("li:last-of-type > input").focus();
         break;
       case (e.target.matches(".addLevel")):
         e.target.parentElement.querySelector(".levels").appendChild(getSnippet('level'));
+        document.querySelector(".level:last-of-type input").focus();
       default:
     }
   });
