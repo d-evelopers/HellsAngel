055fa6eaebfc16562319feb4316b66933173197d
thingywhat <thingywhat@live.com>
2016-02-18 00:04:47 -0700

    Updated tests for Actor and DevilGirl

--- test/test.js
+++ test/test.js
@@ -1,5 +1,33 @@
 var assert = require("chai").assert;
 
+describe("Actor", function(){
+  var Actor = require("../js/Actor");
+  var actor = new Actor();
+
+  it("should be initialized with an empty reaction object", function(){
+    assert.lengthOf(Object.keys(actor.reactions), 0);
+  });
+
+  it("should be able to accept new reactions, and work with them", function(){
+    actor.addReactions({'words': [
+      ["A sentence"]
+    ]});
+
+    assert.lengthOf(Object.keys(actor.reactions), 1);
+    assert.equal(actor.getReaction('words'), "A sentence");
+  });
+
+  it("should be able to accept more than one reaction object", function(){
+    actor.addReactions({'other': [
+      ["Something else"]
+    ]});
+
+    assert.lengthOf(Object.keys(actor.reactions), 2);
+    assert.equal(actor.getReaction('words'), "A sentence");
+    assert.equal(actor.getReaction('other'), "Something else");
+  });
+});
+
 describe("DevilGirl", function(){
   var DevilGirl = require("../js/DevilGirl");
   var devilGirl = new DevilGirl();
@@ -9,23 +37,5 @@ describe("DevilGirl", function(){
     assert.equal(devilGirl.fear, 0);
     assert.equal(devilGirl.affection, 0);
     assert.equal(devilGirl.discipline, 0);
-  });
-
-  it("should be able to accept new reactions, and work with them", function(){
-    devilGirl.addReactions({'words': [
-      ["A sentence"]
-    ]});
-
-    assert.lengthOf(Object.keys(devilGirl.reactions), 1);
-    assert.equal(devilGirl.getReaction('words'), "A sentence");
-  });
-
-  it("should be able to accept more than one reaction object", function(){
-    devilGirl.addReactions({'other': [
-      ["Something else"]
-    ]});
-    assert.lengthOf(Object.keys(devilGirl.reactions), 2);
-    assert.equal(devilGirl.getReaction('words'), "A sentence");
-    assert.equal(devilGirl.getReaction('other'), "Something else");
   });
 });
