ffa9f7c9eec566fc1c187a48305d73371c8c1f8b
thingywhat <thingywhat@live.com>
2016-05-21 16:00:55 -0600

    De-classed display

--- interface/js/display.js
+++ interface/js/display.js
@@ -1,31 +1,30 @@
 'use strict';
 
-module.exports = class Interface {
-  /**
-   * Displays a message in the message dialogue.
-   *
-   * @param <String>: The text to display in the buffer.
-   */
-  static showMessage(text){
-    let messages = document.getElementById("messages");
-    let arrowBox = document.getElementById("arrowbox");
-    let element = document.createElement("p");
+/**
+ * Displays a message in the message dialogue.
+ *
+ * @param <String>: The text to display in the buffer.
+ */
+module.exports.showMessage = function(text){
+  let messages = document.getElementById("messages");
+  let arrowBox = document.getElementById("arrowbox");
+  let element = document.createElement("p");
 
-    element.textContent = text;
-    messages.insertBefore(element, messages.querySelector("p"));
-    messages.className = "visible";
-    arrowBox.className = "";
-  }
+  element.textContent = text;
+  messages.insertBefore(element, messages.querySelector("p"));
+  messages.className = "visible";
+  arrowBox.className = "";
+};
 
-  /**
-   * Hides the message dialogue from the interface..
-   */
-  static hideMessages(){
-    let messages = document.getElementById("messages");
-    let arrowBox = document.getElementById("arrowbox");
 
-    messages.className = "";
-    messages.innerHTML = "";
-    arrowBox.className = "hidden";
-  }
+/**
+ * Hides the message dialogue from the interface..
+ */
+module.exports.hideMessages = function(){
+  let messages = document.getElementById("messages");
+  let arrowBox = document.getElementById("arrowbox");
+
+  messages.className = "";
+  messages.innerHTML = "";
+  arrowBox.className = "hidden";
 };
