b538c99e3f03c1354dca99d7f255d8f30a0dc657
thingywhat <thingywhat@live.com>
2016-05-21 17:01:23 -0600

    Grouped together message components.
    This means that animations can apply to all of them at the same time.
    This is important so we don't, for example, hide the message box, and
    have the name box still sticking around.

--- interface/js/display.js
+++ interface/js/display.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const messages = document.getElementById("messages");
+const messageBox = document.getElementById("messagebox");
 const arrowBox = document.getElementById("arrowbox");
 const nameBox = document.getElementById("namebox");
 
@@ -14,7 +15,7 @@ module.exports.showMessage = function(text){
 
   element.textContent = text;
   messages.insertBefore(element, messages.querySelector("p"));
-  messages.className = "visible";
+  messageBox.className = "visible";
   arrowBox.className = "";
 };
 
@@ -22,7 +23,7 @@ module.exports.showMessage = function(text){
  * Hides the message dialogue from the interface..
  */
 module.exports.hideMessages = function(){
-  messages.className = "";
+  messageBox.className = "";
   messages.innerHTML = "";
   arrowBox.className = "hidden";
   nameBox.className = "hidden";
