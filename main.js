316a6aba543a8c698181d8506a32cf5b687ddc32
thingywhat <thingywhat@gmail.com>
2016-08-27 13:27:18 -0600

    Organized and documented main.js
    Also, the dev tools not have access to the window object of the main
    process.

--- main.js
+++ main.js
@@ -19,27 +19,10 @@ const devMode = !!(process.argv.filter(function(arg){
 // Read in the configuration
 const config = JSON.parse(fs.readFileSync("config.json"));
 
-app.on('ready', function(){
-  var window = new BrowserWindow({
-    'width': config.width,
-    'height': config.height
-  });
-
-  if(devMode){
-  } else {
-    window.setMenu(null);
-  }
-
-  window.loadURL("file://" + __dirname + "/interface/interface.html");
-
-  window.on('closed', function(){
-    exports.window = null;
-  });
-
-  // If we require this file, we can play with the window instance.
-  exports.window = window;
-});
-
+/*
+ * When we get a request-script request-script request from the
+ * client, we want to try and read it in and send the contents back.
+ */
 ipc.on("request-script", function(e, script){
   fs.readFile(path.join(__dirname, "scripts", script.replace(/\.{2,}/g, ".")), function(err, data){
     if(!err){
@@ -50,16 +33,46 @@ ipc.on("request-script", function(e, script){
   });
 });
 
+/*
+ * When we get a "flags" request, we will send back an object
+ * containing a select number of flags from the main process
+ */
 ipc.on("flags", function(e){
   e.sender.send("flags", {
     'devMode': devMode
   });
 });
 
-if(devMode){
-  require('./dev-tools')({
-    'app': app,
-    'ipc': ipc,
-    'BrowserWindow': BrowserWindow
   });
-}
+
+/*
+ * Loads the main game window, and calls back anything that tried to
+ * require this file with it
+ */
+app.on('ready', function(){
+  let window = new BrowserWindow({
+    'width': config.width,
+    'height': config.height
+  });
+
+  /*
+   * In the event the user is in dev mode, drop to the REPL in
+   * dev-tools.js
+   */
+  if(devMode){
+    require('./dev-tools')({
+      'app': app,
+      'ipc': ipc,
+      'window': window,
+      'BrowserWindow': BrowserWindow
+    });
+  } else {
+    window.setMenu(null);
+  }
+
+  window.loadURL("file://" + __dirname + "/interface/interface.html");
+
+  window.on('closed', function(){
+    window = null;
+  });
+});
